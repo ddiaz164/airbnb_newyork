55099d3bbde20ac53a7b50289d889ab53881c98c
David Diaz <daviddiaz10.19@gmail.com>
2020-05-11 11:34:39 -0700

    progress

---
+++ src/model_helpers.py
@@ -0,0 +1,91 @@
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import numpy as np
+import textwrap
+
+def cross_val(estimator, X_train, y_train, nfolds):
+
+    mse = cross_val_score(estimator, X_train, y_train, 
+                          scoring='neg_mean_squared_error',
+                          cv=nfolds, n_jobs=-1) * -1
+
+    r2 = cross_val_score(estimator, X_train, y_train, 
+                         scoring='r2', cv=nfolds, n_jobs=-1)
+    mean_mse = mse.mean()
+    mean_r2 = r2.mean()
+    name = estimator.__class__.__name__
+    print("{0:<25s} Train CV | MSE: {1:0.3f} | R2: {2:0.3f}".format(name,
+                                                        mean_mse, mean_r2))
+    return mean_mse, mean_r2
+    
+def stage_score_plot(estimator, X_train, y_train, X_test, y_test):
+
+    estimator.fit(X_train, y_train)
+    name = estimator.__class__.__name__.replace('Regressor', '')
+    learn_rate = estimator.learning_rate
+
+    train_scores = np.zeros((estimator.n_estimators,), dtype=np.float64)
+    test_scores = np.zeros((estimator.n_estimators,), dtype=np.float64)
+
+    for i, y_train_pred in enumerate(estimator.staged_predict(X_train)):
+        train_scores[i] = mean_squared_error(y_train, y_train_pred)
+
+    for i, y_test_pred in enumerate(estimator.staged_predict(X_test)):
+        test_scores[i] = mean_squared_error(y_test, y_test_pred)
+    plt.plot(train_scores, alpha=.5, label="{0} Train - learning rate {1}".format(
+                                                                name, learn_rate))
+    plt.plot(test_scores, alpha=.5, label="{0} Test  - learning rate {1}".format(
+                                                      name, learn_rate), ls='--')
+    plt.title(name, fontsize=16, fontweight='bold')
+    plt.ylabel('MSE', fontsize=14)
+    plt.xlabel('Iterations', fontsize=14)
+
+def rf_score_plot(randforest, X_train, y_train, X_test, y_test):
+
+    randforest.fit(X_train, y_train)
+    y_test_pred = randforest.predict(X_test)
+    test_score = mean_squared_error(y_test, y_test_pred)
+    plt.axhline(test_score, alpha = 0.7, c = 'y', lw=3, ls='-.', label = 
+                                                        'Random Forest Test')
+
+def gridsearch_with_output(estimator, parameter_grid, X_train, y_train):
+
+    model_gridsearch = GridSearchCV(estimator,
+                                    parameter_grid,
+                                    n_jobs=-1,
+                                    verbose=True,
+                                    scoring='neg_mean_squared_error')
+    model_gridsearch.fit(X_train, y_train)
+    best_params = model_gridsearch.best_params_ 
+    model_best = model_gridsearch.best_estimator_
+    print("\nResult of gridsearch:")
+    print("{0:<20s} | {1:<8s} | {2}".format("Parameter", "Optimal", "Gridsearch values"))
+    print("-" * 55)
+    for param, vals in parameter_grid.items():
+        print("{0:<20s} | {1:<8s} | {2}".format(str(param), 
+                                                str(best_params[param]),
+                                                str(vals)))
+    return best_params, model_best
+
+
+
+def display_default_and_gsearch_model_results(model_default, model_gridsearch, 
+                                              X_test, y_test):
+
+    name = model_default.__class__.__name__.replace('Regressor', '') # for printing
+    y_test_pred = model_gridsearch.predict(X_test)
+    mse = mean_squared_error(y_test, y_test_pred)
+    r2 = r2_score(y_test, y_test_pred)
+    print("Results for {0}".format(name))
+    print("Gridsearched model mse: {0:0.3f} | r2: {1:0.3f}".format(mse, r2))
+    y_test_pred = model_default.predict(X_test)
+    mse = mean_squared_error(y_test, y_test_pred)
+    r2 = r2_score(y_test, y_test_pred)
+    print("     Default model mse: {0:0.3f} | r2: {1:0.3f}".format(mse, r2))
+
---
+++ src/nlp_helpers.py
@@ -0,0 +1,82 @@
+
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF as NMF_sklearn
+def text_vectorizer(contents, use_tfidf=True, use_stemmer=False, max_features=None):
+    Vectorizer = TfidfVectorizer if use_tfidf else CountVectorizer
+    tokenizer = RegexpTokenizer(r"[\w']+")
+    stem = PorterStemmer().stem if use_stemmer else (lambda x: x)
+    stop_set = set(stopwords.words('english'))
+    def tokenize(text):
+        tokens = tokenizer.tokenize(text)
+        stems = [stem(token) for token in tokens if token not in stop_set]
+        return stems
+    vectorizer_model = Vectorizer(tokenizer=tokenize, max_features=max_features)
+    vectorizer_model.fit(contents)
+    vocabulary = np.array(vectorizer_model.get_feature_names())
+    def vectorizer(X):
+        return vectorizer_model.transform(X).toarray()
+    return vectorizer, vocabulary
+def softmax(v, temperature=1.0):
+    expv = np.exp(v / temperature)
+    s = np.sum(expv)
+    return expv / s
+def hand_label_topics(H, vocabulary):
+    hand_labels = []
+    for i, row in enumerate(H):
+        top_five = np.argsort(row)[::-1][:20]
+        print('topic', i)
+        print('-->', ' '.join(vocabulary[top_five]))
+        label = input('topic: ')
+        hand_labels.append(label)
+        print()
+    return hand_labels
+def analyze(article_index, contents, web_urls, W, hand_labels):
+    print(web_urls[article_index])
+    print(contents[article_index])
+    probs = softmax(W[article_index], temperature=0.01)
+    for prob, label in zip(probs, hand_labels):
+        print('--> {:.2f}% {}'.format(prob * 100, label))
+    print()
+def scree_plot(ax, pca, n_components_to_plot=8, title=None):
+
+    num_components = pca.n_components_
+    ind = np.arange(num_components)
+    vals = pca.explained_variance_ratio_
+    ax.plot(ind, vals, color='blue')
+    ax.scatter(ind, vals, color='blue', s=50)
+    for i in range(num_components):
+        ax.annotate(r"{:2.2f}%".format(vals[i]), 
+                   (ind[i]+0.2, vals[i]+0.005), 
+                   va="bottom", 
+                   ha="center", 
+                   fontsize=12)
+    ax.set_xticklabels(ind, fontsize=12)
+    ax.set_ylim(0, max(vals) + 0.05)
+    ax.set_xlim(0 - 0.45, n_components_to_plot + 0.45)
+    ax.set_xlabel("Principal Component", fontsize=12)
+    ax.set_ylabel("Variance Explained (%)", fontsize=12)
+    if title is not None:
+        ax.set_title(title, fontsize=16)
+def plot_name_embedding(ax, X, y, title=None):
+
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+    ax.axis('off')
+    ax.patch.set_visible(False)
+    for i in range(X.shape[0]):
+        plt.text(X[i, 0], X[i, 1], 
+                 str(digits.target[i]), 
+                 color=plt.cm.Set1(y[i] / 10.), 
+                 fontdict={'weight': 'bold', 'size': 12})
+    ax.set_xticks([]), 
+    ax.set_yticks([])
+    ax.set_ylim([-0.1,1.1])
+    ax.set_xlim([-0.1,1.1])
+    if title is not None:
+        ax.set_title(title, fontsize=16)
+        
